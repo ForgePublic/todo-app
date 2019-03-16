df413033919c5067681162798723498b2cca795c
ForgePublic <rami.nao.2411@gmail.com>
2019-03-16 20:01:50 +0900

    todo-app完成

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-main.js
+var app = new Vue({
+	el: "#app",
+	data: {
+		newItem: '',
+		todos: []
+	},
+	methods: {
+		addItem: function (event) {
+			if(this.newItem == '') return;
+
+			var todo = {
+				item: this.newItem,
+				isDone: false
+			};
+
+			this.todos.push(todo);
+			this.newItem = '';
+		},
+		deleteItem: function (index) {
+			this.todos.splice(index, 1)
+		}
+	}
+})	
